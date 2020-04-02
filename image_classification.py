cdde4c6aad7703eb2cc8da2a1611b155c684f237
mitya <dmitriy.akhmediyev@gmail.com>
2020-04-02 16:13:49 +0300

    script for manually testing the model

---
+++ image_classification.py
@@ -0,0 +1,54 @@
+from keras.preprocessing.image import img_to_array
+import imutils
+import cv2
+from keras.models import load_model
+import numpy as np
+import tensorflow as tf
+
+# uncoment this if you using gpu
+#physical_devices = tf.config.experimental.list_physical_devices('GPU')
+#assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+#config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# parameters for loading data and images
+detection_model_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
+emotion_model_path = 'models/_mini_XCEPTION.73-0.64.hdf5'  # path to your model
+image_path = 'image7.jpg'   # path to image that you want to classify
+
+# hyper-parameters for bounding boxes shape
+# loading models
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["angry", "disgust", "scared", "happy", "sad", "surprised",
+            "neutral"]
+
+
+
+frame = cv2.imread(image_path)
+    # reading the frame
+frame = imutils.resize(frame, width=300)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+face = face_detection.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+                                        flags=cv2.CASCADE_SCALE_IMAGE)
+
+canvas = np.zeros((250, 300, 3), dtype="uint8")
+frameClone = frame.copy()
+
+if len(face) > 0:
+    faces = sorted(face, reverse=True,
+                   key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+    (fX, fY, fW, fH) = faces
+    # Extract the ROI of the face from the grayscale image, resize it to a fixed 28x28 pixels, and then prepare
+    # the ROI for classification via the CNN
+    roi = gray[fY:fY + fH, fX:fX + fW]
+    roi = cv2.resize(roi, (48, 48))
+    roi = roi.astype("float") / 255.0
+    roi = img_to_array(roi)
+    roi = np.expand_dims(roi, axis=0)
+
+    preds = emotion_classifier.predict(roi)[0]
+    emotion_probability = np.max(preds)
+    label = EMOTIONS[preds.argmax()]
+
+    print(label)
+
