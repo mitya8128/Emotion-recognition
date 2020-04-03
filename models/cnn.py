1490915724b700e7712ab5bf3d61bc400986b9ec
mitya <dmitriy.akhmediyev@gmail.com>
2020-04-03 16:23:26 +0300

    use_bias=True (big_XCEPTION)

--- models/cnn.py
+++ models/cnn.py
@@ -297,35 +297,35 @@ def mini_XCEPTION(input_shape, num_classes, l2_regularization=0.01):
 
 def big_XCEPTION(input_shape, num_classes):
     img_input = Input(input_shape)
-    x = Conv2D(32, (3, 3), strides=(2, 2), use_bias=False)(img_input)
+    x = Conv2D(32, (3, 3), strides=(2, 2), use_bias=True)(img_input)
     x = BatchNormalization(name='block1_conv1_bn')(x)
     x = Activation('relu', name='block1_conv1_act')(x)
-    x = Conv2D(64, (3, 3), use_bias=False)(x)
+    x = Conv2D(64, (3, 3), use_bias=True)(x)
     x = BatchNormalization(name='block1_conv2_bn')(x)
     x = Activation('relu', name='block1_conv2_act')(x)
 
     residual = Conv2D(128, (1, 1), strides=(2, 2),
-                      padding='same', use_bias=False)(x)
+                      padding='same', use_bias=True)(x)
     residual = BatchNormalization()(residual)
 
-    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False)(x)
+    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=True)(x)
     x = BatchNormalization(name='block2_sepconv1_bn')(x)
     x = Activation('relu', name='block2_sepconv2_act')(x)
-    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False)(x)
+    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=True)(x)
     x = BatchNormalization(name='block2_sepconv2_bn')(x)
 
     x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
     x = layers.add([x, residual])
 
     residual = Conv2D(256, (1, 1), strides=(2, 2),
-                      padding='same', use_bias=False)(x)
+                      padding='same', use_bias=True)(x)
     residual = BatchNormalization()(residual)
 
     x = Activation('relu', name='block3_sepconv1_act')(x)
-    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False)(x)
+    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=True)(x)
     x = BatchNormalization(name='block3_sepconv1_bn')(x)
     x = Activation('relu', name='block3_sepconv2_act')(x)
-    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False)(x)
+    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=True)(x)
     x = BatchNormalization(name='block3_sepconv2_bn')(x)
 
     x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
